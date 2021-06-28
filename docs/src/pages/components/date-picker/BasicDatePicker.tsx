5ce386a7de494f5bdd2359e85d060b84bd8e2b0c
Michał Perłakowski <michal.perlakowski@toolla.net>
2021-06-28 12:01:05 +0200

    Toolbar type error demo

--- docs/src/pages/components/date-picker/BasicDatePicker.tsx
+++ docs/src/pages/components/date-picker/BasicDatePicker.tsx
@@ -3,6 +3,19 @@ import TextField from '@material-ui/core/TextField';
 import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
 import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
 import DatePicker from '@material-ui/lab/DatePicker';
+// eslint-disable-next-line no-restricted-imports
+import PickersToolbar from '@material-ui/lab/internal/pickers/PickersToolbar';
+// eslint-disable-next-line no-restricted-imports
+import { ToolbarComponentProps } from '@material-ui/lab/internal/pickers/typings/BasePicker';
+
+function ToolbarComponent(props: ToolbarComponentProps<Date>) {
+  return (
+    <PickersToolbar
+      toolbarTitle=""
+      {...props}
+    />
+  );
+}
 
 export default function BasicDatePicker() {
   const [value, setValue] = React.useState<Date | null>(null);
@@ -16,6 +29,7 @@ export default function BasicDatePicker() {
           setValue(newValue);
         }}
         renderInput={(params) => <TextField {...params} />}
+        ToolbarComponent={ToolbarComponent}
       />
     </LocalizationProvider>
   );
